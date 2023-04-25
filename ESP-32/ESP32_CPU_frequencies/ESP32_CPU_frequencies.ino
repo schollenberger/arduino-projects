fc8aedef50bfa28b80c887de7727a0666e05f6d5
schollenberger <w.schollenberger@gmail.com>
2023-04-25 18:14:40 +0200

    merge ESP32 CPU freq projects as they have been very similar

--- ESP-32/ESP32_CPU_frequencies/ESP32_CPU_frequencies.ino
+++ ESP-32/ESP32_CPU_frequencies/ESP32_CPU_frequencies.ino
@@ -13,47 +13,71 @@
  */
  
 #include "Arduino.h"
- 
+
+
+void printBothSerialLines(char* txt) {
+  Serial.print(txt);
+  Serial2.print(txt);
+}
+
+void printlnBothSerialLines(char* txt) {
+  Serial.println(txt);
+  Serial2.println(txt);
+  
+}
+
+void printCpuFrequency() 
+{
+  char buf[10];
+  printBothSerialLines("CPU Freq: ");
+  sprintf(buf, "%d", getCpuFrequencyMhz());  
+  printlnBothSerialLines(buf);
+}
+
+
 void setup()
 {
     Serial.begin(115200);
- 
-    while (!Serial){
+    Serial2.begin(115200);
+    while (!Serial){    // mainly required for Arduino boards
         delay(500);
     }
-
+    while (!Serial2){
+        delay(500);
+    }
+    delay(500); // always give 500 ms to initialize for ESP boards
+    
     int waitTime = 20;
-    Serial.print("CPU Freq: ");
-    Serial.println(getCpuFrequencyMhz());
+
+    char buf[10];
+    
+    printBothSerialLines("Initial CPU Freq: ");
+    sprintf(buf, "%d", getCpuFrequencyMhz());
+    printlnBothSerialLines(buf);
     delay(waitTime*1000);
     
     setCpuFrequencyMhz(160);
-    Serial.print("CPU Freq: ");
-    Serial.println(getCpuFrequencyMhz());
+    printCpuFrequency();
     delay(waitTime*1000);
     
     setCpuFrequencyMhz(80);
-    Serial.print("CPU Freq: ");
-    Serial.println(getCpuFrequencyMhz());
+    printCpuFrequency();
     delay(waitTime*1000);
     
     setCpuFrequencyMhz(40);
-    Serial.print("CPU Freq: ");
-    Serial.println(getCpuFrequencyMhz());
+    printCpuFrequency();
     delay(waitTime*1000);
     
     setCpuFrequencyMhz(20);
-    Serial.print("CPU Freq: ");
-    Serial.println(getCpuFrequencyMhz());
+    printCpuFrequency();
     delay(waitTime*1000);
     
     setCpuFrequencyMhz(10);
-    Serial.print("CPU Freq: ");
-    Serial.println(getCpuFrequencyMhz());
+    printCpuFrequency();
     delay(waitTime*1000);
 }
  
 void loop()
 {
- 
+     delay(1000); 
 }
